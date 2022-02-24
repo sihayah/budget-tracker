26b3c1b5b09b39465bd93762b82f0f55f254b699
Sihayah <sihaya_a@hotmail.com>
2022-02-24 14:05:01 -0500

    add variables and files to cache to service worker

---
+++ public/service-worker.js
@@ -0,0 +1,17 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+    "./public/index.html",
+    "./public/css/style.css",
+    "./public/js/idb.js",
+    "./public/js/index.js",
+    "./public/icons/icon-72x72.png",
+    "./public/icons/icon-96x96.png",
+    "./public/icons/icon-128x128.png",
+    "./public/icons/icon-144x144.png",
+    "./public/icons/icon-152x152.png",
+    "./public/icons/icon-192x192.png",
+    "./public/icons/icon-384x384.png",
+    "./public/icons/icon-512x512.png"
+]
