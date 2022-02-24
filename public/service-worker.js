a4c38abb7aefa3c31a295ec4ea385b64120b4dc9
Sihayah <sihaya_a@hotmail.com>
2022-02-24 15:08:39 -0500

    fixed rel path to manifest.json in index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,7 +9,6 @@ const FILES_TO_CACHE = [
     "./public/icons/icon-72x72.png",
     "./public/icons/icon-96x96.png",
     "./public/icons/icon-128x128.png",
-    "./public/icons/icon-144x144.png",
     "./public/icons/icon-152x152.png",
     "./public/icons/icon-192x192.png",
     "./public/icons/icon-384x384.png",
@@ -30,7 +29,7 @@ self.addEventListener('fetch', function(e) {
             }
         })
     )
-});s
+});
 
 // Cache resources
 self.addEventListener('install', function(e) {
